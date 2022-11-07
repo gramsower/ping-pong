5d8572f7ab4f3396640e70408e496aa8f2c817f5
gramsower <110431307+gramsower@users.noreply.github.com>
2022-11-06 16:58:07 -0800

    initial commit after full day of coding, WIP with unsolved errors preventing build

---
+++ program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Replace.Number;
+
+namespace Statement
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Please enter a number: ");
+      string userNumber = Console.ReadLine();
+      private int _userNumber = int.Parse(userNumber);
+    }
+  }
+}
